aba020468535253592ffd102e396cf615fd3ce40
Eduardo-Amaral <eduardo.lopes.amaralxx@gmail.com>
2022-01-11 23:43:42 -0300

    MovieCard

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,8 +1,8 @@
-import {ReactComponent as GithubIcon} from 'assets/img/github.svg'
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg'
 import './styles.css';
-function Navbar(){
-    return(
-        <header>
+function Navbar() {
+  return (
+    <header>
       <nav className='container'>
         <div className='dsmovie-nav-content'>
           <h1>DSMovie</h1>
@@ -15,7 +15,7 @@ function Navbar(){
         </div>
       </nav>
     </header>
-    );
+  );
 }
 
 export default Navbar;
